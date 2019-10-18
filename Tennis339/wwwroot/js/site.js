5c6f4474c91d6453b81b3e1a2c66aee68cb79038
Gia <33999927+QuantumPhysx2@users.noreply.github.com>
2019-10-18 11:07:26 +0930

    Fixing mobile menu

--- Tennis339/wwwroot/js/site.js
+++ Tennis339/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification for details on configuring this project to bundle and minify static web assets.
+
+var toggler = document.querySelector(".navbar-toggler"); 
+var menu = document.querySelector(".navbar-collapse");
+
+toggler.addEventListener("click", function () {
+
+    if (menu.style.display == "unset") {
+        menu.style.display = "none";
+    } 
+    else {
+        menu.style.display = "unset";
+    }
+});
